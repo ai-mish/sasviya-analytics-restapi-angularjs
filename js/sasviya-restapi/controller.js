4f83c68405e44b0d7a7de57af099bca1693b3197
Mudit Mishra <mudit.mishra@sas.com>
2018-03-29 11:01:36 +1100

    html update

--- js/sasviya-restapi/controller.js
+++ js/sasviya-restapi/controller.js
@@ -4,6 +4,7 @@ angular.module('loanApp')
 .controller('loanController', ['$scope','$httpParamSerializer','$http','$window','loanService',
 function ($scope,$httpParamSerializer,$http,$window,loanService) {
 
+
   var decisionPublishedName = 'hmeq_approval1_0';
   var input_data_json = {
     "version": 1,
@@ -15,16 +16,28 @@ function ($scope,$httpParamSerializer,$http,$window,loanService) {
     ]};
 
   function run(decisionPublishedName,input_data_json){
+    $scope.modelProcessing = true
     loanService.connect()
         .then(function(data) {
-          console.log(data)
-          $window.sessionStorage.token = data.token;
+          console.log(data);
+          console.log('Token:');
+          $window.sessionStorage.token = data.access_token;
           $scope.message = 'Welcome';
           loanService.executeDecision(decisionPublishedName,input_data_json).
-          then(function(response){console.log(response)},function(error){console.log(error)})
+          then(function(response){
+            $scope.modelProcessing = false;
+            $scope.scoreResult=response.outputs["Approve"];
+            console.log(response)
+          },
+          function(error)
+          {
+            console.log(error);
+            $scope.modelProcessing = false;
+          })
         }, function(error){
           console.error(error);
           delete $window.sessionStorage.token;
+          $scope.modelProcessing = false;
           $scope.message = 'Error: Invalid user or password';
         });
   }
