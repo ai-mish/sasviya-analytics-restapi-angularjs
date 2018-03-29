a1abf6a778dc8777e91287ed8d7be5a4580ccdbc
Mudit Mishra <mudit.mishra@sas.com>
2018-03-29 11:53:29 +1100

    dynamic input

--- js/sasviya-restapi/controller.js
+++ js/sasviya-restapi/controller.js
@@ -4,18 +4,13 @@ angular.module('loanApp')
 .controller('loanController', ['$scope','$httpParamSerializer','$http','$window','loanService',
 function ($scope,$httpParamSerializer,$http,$window,loanService) {
 
+  function runDecision(decisionPublishedName,input_data_json){
 
-  var decisionPublishedName = 'hmeq_approval1_0';
-  var input_data_json = {
-    "version": 1,
-    "inputs": [
-      {"name": "CLNO","value": 1.0},
-      {"name": "JOB","value": "IT"},
-      {"name": "REASON","value": "1"},
-      {"name": "VALUE","value": 2.0}
-    ]};
 
-  function run(decisionPublishedName,input_data_json){
+
+    if ( $window.sessionStorage.token ) {
+    delete $window.sessionStorage.token;
+    }
     $scope.modelProcessing = true
     loanService.connect()
         .then(function(data) {
@@ -26,7 +21,9 @@ function ($scope,$httpParamSerializer,$http,$window,loanService) {
           loanService.executeDecision(decisionPublishedName,input_data_json).
           then(function(response){
             $scope.modelProcessing = false;
-            $scope.scoreResult=response.outputs["Approve"];
+            console.log("Response");
+            $scope.Approve=response["outputs"][0]["value"];
+            $scope.P_BAD1=response["outputs"][1]["value"];
             console.log(response)
           },
           function(error)
@@ -42,6 +39,28 @@ function ($scope,$httpParamSerializer,$http,$window,loanService) {
         });
   }
 
- run(decisionPublishedName,input_data_json);
+$scope.run = function() {
+    var CLNO=$scope.CLNO;
+    var JOB = $scope.JOB;
+    var REASON = $scope.REASON;
+    var VALUE = $scope.VALUE;
+    var decisionPublishedName = 'hmeq_approval1_0';
+    var input_data_json = {
+      "version": 1,
+      "inputs": [
+        {"name": "CLNO","value": 1.0},
+        {"name": "JOB","value": "IT"},
+        {"name": "REASON","value": "1"},
+        {"name": "VALUE","value": 2.0}
+      ]};
+
+      input_data_json["inputs"][0]["value"]=CLNO;
+      input_data_json["inputs"][1]["value"]=JOB;
+      input_data_json["inputs"][2]["value"]=REASON;
+      input_data_json["inputs"][3]["value"]=VALUE;
+
+      console.log(input_data_json);
+      runDecision(decisionPublishedName,input_data_json);
+    }
 
 }]);
